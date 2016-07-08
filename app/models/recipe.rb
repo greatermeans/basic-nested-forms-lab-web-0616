5bbbe966dfd71f9c83c7b0bae77099b78ea69462
greatermeans <gakouri@gmail.com>
2016-07-08 13:32:34 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+
+  def ingredients_attributes=(ingredients)
+  	ingredients.values.each do |ingredient|
+  		self.ingredients << Ingredient.create(ingredient) if ingredient[:name].present?
+  	end
+  end
+  
 end
